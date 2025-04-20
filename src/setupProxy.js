77a1771a280c1cb723d34841375c13308d94b30c
Manoj99Q <gmanojcs9@gmail.com>
2025-04-20 17:06:43 -0500

    cloud url

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,16 +3,13 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 /*
 This acts a proxy between the react application and the flask microservice
 Everytime there is a request to /api, the setupProxy prepends the flask
-microservice url mentioned in line 14
+microservice url
 */
 module.exports = function (app) {
-  // Check if we're in development mode
-  const isDevelopment = process.env.NODE_ENV === "development";
+  // Directly use the deployed Flask URL for cloud testing
+  const targetUrl = "https://flask-asign5-323579180404.us-central1.run.app";
 
-  // Use flask-service (Docker service name) in development, otherwise use cloud URL
-  const targetUrl = isDevelopment
-    ? "http://flask-service:5000"
-    : "https://flask-asign5-323579180404.us-central1.run.app";
+  console.log("Using Flask API URL:", targetUrl);
 
   app.use(
     "/api",
@@ -21,16 +18,4 @@ module.exports = function (app) {
       changeOrigin: true,
     })
   );
-
-  // Add proxy for static image files from LSTM service
-  // This routes requests for /static/images/* to the LSTM service
-  if (isDevelopment) {
-    app.use(
-      "/static/images",
-      createProxyMiddleware({
-        target: "http://lstm-service:8080",
-        changeOrigin: true,
-      })
-    );
-  }
 };
